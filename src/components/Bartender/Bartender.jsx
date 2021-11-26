5c68e21a2b551c32875aef2044eddd228d0e3121
Miguel <migu0403@stud.kea.dk>
2021-11-26 21:57:48 +0100

    depot

--- src/components/Bartender/Bartender.jsx
+++ src/components/Bartender/Bartender.jsx
@@ -11,11 +11,29 @@ function TimeToClose(props) {
   );
 }
 
+function Keg(props) {
+  return (
+    <li>
+      {props.name}: {props.amount} left
+    </li>
+  );
+}
+
+function Depot(props) {
+  const kegs = props.storage.map((keg, i) => <Keg key={(i < 9 ? "keg0" : "keg") + (i + 1)} {...keg} />);
+  return (
+    <section className="Depot">
+      <ul>{kegs}</ul>
+    </section>
+  );
+}
+
 export default function Bartender(props) {
   return (
     <div className="Bartender">
-      <Taps bartenders={props.bartenders} taps={props.taps} />
       <TimeToClose time={props.time} />
+      <Taps bartenders={props.bartenders} taps={props.taps} />
+      <Depot {...props} />
     </div>
   );
 }
