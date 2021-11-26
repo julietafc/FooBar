f9ff4ea0e507ea562b372de68b4c3136d588ea2a
Miguel <migu0403@stud.kea.dk>
2021-11-26 12:17:11 +0100

    bulbs working

--- src/components/Bartender/Bartender.jsx
+++ src/components/Bartender/Bartender.jsx
@@ -1,8 +1,10 @@
 import "./Bartender.scss";
+import Taps from "../helpers/Taps/Taps";
 
-export default function Bartender() {
+export default function Bartender(props) {
   return (
     <div className="Bartender">
+      <Taps bartenders={props.bartenders} taps={props.taps} />
       <h1>Content for Bartenders</h1>
     </div>
   );
