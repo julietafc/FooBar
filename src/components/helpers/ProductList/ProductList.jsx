6fa9c075d80fff37f5ba8e8f73b16bcf54239fe2
Miguel <migu0403@stud.kea.dk>
2021-11-26 21:11:57 +0100

    keys

--- src/components/helpers/ProductList/ProductList.jsx
+++ src/components/helpers/ProductList/ProductList.jsx
@@ -2,7 +2,7 @@ import Product from "./Product/Product";
 import "./ProductList.scss";
 
 export default function ProductList(props) {
-  const beers = props.beers.map((beer) => <Product {...beer} />);
+  const beers = props.beers.map((beer) => <Product key={beer.id} {...beer} />);
   return (
     <section className="ProductList">
       <h2>Our selection</h2>
