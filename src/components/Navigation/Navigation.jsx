99dedf5e16a040a4276d884e2f481a0d6eb108f3
julietafc <juli47g1@stud.kea.dk>
2021-11-27 14:45:37 +0100

    starting navigation component

---
+++ src/components/Navigation/Navigation.jsx
@@ -0,0 +1,10 @@
+export default function Navigation() {
+  return (
+    <nav>
+      <a href="#">Manager</a>
+      <a href="#">Bartenders</a>
+      <a href="#">Customers</a>
+      <a href="#">Order</a>
+    </nav>
+  );
+}
