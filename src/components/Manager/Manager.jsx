ad304bbce74e586424804cea8fbc42e120d22b13
CW Munch <chri779r@stud.kea.dk>
2021-11-29 20:39:59 +0100

    inventory now stands as a component on it's own

--- src/components/Manager/Manager.jsx
+++ src/components/Manager/Manager.jsx
@@ -1,9 +1,19 @@
 import Clock from "../helpers/Time/Time";
+import Inventory from "../helpers/Inventory/Inventory";
 
-export default function Manager() {
+export default function Manager(props) {
 
 
     return(
-    <Clock/>
+        <>
+      <div>
+        <h2>The bar will close down at 22.00</h2>
+        <Clock/>
+      </div>
+      
+      <div>
+        <Inventory {...props}/>
+      </div>
+      </>
     )
 }
