eaf9979d9de82604ff4a71a2dc5c940e5e2791e0
Miguel <migu0403@stud.kea.dk>
2021-11-29 15:22:03 +0100

    order list

--- src/components/helpers/OrderList/OrderList.jsx
+++ src/components/helpers/OrderList/OrderList.jsx
@@ -1,7 +1,21 @@
 import { useState } from "react";
 
 export default function OrderList(props) {
-  const ordersToDo = props.queue.length < 1 ? <li>no orders</li> : props.queue.map((order) => <li key={order.id}>{order.id} </li>);
+  const ordersToDo =
+    props.queue.length < 1 ? (
+      <li>no orders</li>
+    ) : (
+      props.queue.map((order) => (
+        <li key={order.id}>
+          {order.id}{" "}
+          <ul>
+            {order.order.map((beer, i) => (
+              <li key={order.id + "b" + i}>{beer}</li>
+            ))}
+          </ul>
+        </li>
+      ))
+    );
   const ordersServing = props.serving.map((order) => <li key={"s" + order.id}>{order.id}</li>);
   return (
     <aside className="OrderList">
