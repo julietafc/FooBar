c2034cf16047a5c01dbbe8ecd473ad3c1fb17168
CW Munch <chri779r@stud.kea.dk>
2021-12-01 21:04:24 +0100

    ranking now works

--- src/components/helpers/Ranking/Ranking.jsx
+++ src/components/helpers/Ranking/Ranking.jsx
@@ -1,14 +1,14 @@
 import "./Ranking.scss";
 
 export default function TopBeer(props) {
-    const ranking = props.storage;
-    const rankingMap = ranking.map((keg, i ) => <li key={i}>{keg.name}</li> );
-    let rankingSort = rankingMap.sort((a,b)=>a.keg > b.keg)
+    const ranking = [...props.storage];
+    let rankingSort = ranking.sort((a, b)=>a.amount-b.amount);
+    const rankingMap = rankingSort.map((keg, i ) => <li key={i}>{keg.name}</li> );
 
     return (
         <div>
             <h3>Top Selling Beers</h3>
-            <ul>{rankingSort}</ul>
+            <ul>{rankingMap}</ul>
         </div>
     )
 
