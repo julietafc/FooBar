93da7da313e1d43b5f323460f1d75ed5494c3ed4
julietafc <juli47g1@stud.kea.dk>
2021-12-03 22:53:22 +0100

    Footer component added

---
+++ src/components/helpers/Footer/Footer.jsx
@@ -0,0 +1,8 @@
+import "./Footer.scss";
+export default function Footer() {
+  return (
+    <footer>
+      <a href="https://www.freepik.com/vectors/background">Background vector created by pikisuperstar - www.freepik.com</a>
+    </footer>
+  );
+}
