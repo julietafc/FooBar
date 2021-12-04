1308c47b6d26bef93bff49988edbddea62eaf29e
julietafc <juli47g1@stud.kea.dk>
2021-12-04 16:11:03 +0100

    starting sorting

--- src/components/helpers/ProductList/ProductList.jsx
+++ src/components/helpers/ProductList/ProductList.jsx
@@ -1,11 +1,33 @@
+import { useState } from "react";
 import Product from "./Product/Product";
 import "./ProductList.scss";
 
 export default function ProductList(props) {
   const beers = props.beers.map((beer) => <Product addToBasket={props.addToBasket} addMoreBeer={props.addMoreBeer} key={beer.id} {...beer} />);
+  // const [filter, setFilter] = useState(false);
+  const [sort, setSort] = useState("name");
+  const [dir, setDir] = useState("asc");
+  let displayList = [...beers];
+  // if (filter) {
+  //   displayList = beers.filter((beer) => beer.category === filter);
+  // }
+
+  function sortBeers() {
+    displayList.sort((a, b) => {
+      if (a[sort] > b[sort]) {
+        return dir === "asc" ? 1 : -1;
+      } else {
+        return dir === "asc" ? -1 : 1;
+      }
+    });
+  }
+  sortBeers();
   return (
     <section className="ProductList">
       <h2>Our selection</h2>
+      <button onClick={() => setSort("name")}>Name</button>
+      <button onClick={() => setSort("category")}>Type</button>
+      <button onClick={() => setSort("alcohol")}>% alcohol</button>
       <section>{beers}</section>
     </section>
   );
