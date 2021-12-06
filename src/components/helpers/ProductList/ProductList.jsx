ad4943271d10c130a635bae9c8d8f919e5b213c8
julietafc <juli47g1@stud.kea.dk>
2021-12-06 20:27:44 +0100

    sorting working

--- src/components/helpers/ProductList/ProductList.jsx
+++ src/components/helpers/ProductList/ProductList.jsx
@@ -3,31 +3,49 @@ import Product from "./Product/Product";
 import "./ProductList.scss";
 
 export default function ProductList(props) {
-  const beers = props.beers.map((beer) => <Product addToBasket={props.addToBasket} addMoreBeer={props.addMoreBeer} key={beer.id} {...beer} />);
-  // const [filter, setFilter] = useState(false);
-  const [sort, setSort] = useState("name");
-  const [dir, setDir] = useState("asc");
-  let displayList = [...beers];
-  // if (filter) {
-  //   displayList = beers.filter((beer) => beer.category === filter);
-  // }
+  const [sortBy, setSortBy] = useState("name");
+  const [direction, setDirection] = useState(1);
 
-  function sortBeers() {
-    displayList.sort((a, b) => {
-      if (a[sort] > b[sort]) {
-        return dir === "asc" ? 1 : -1;
-      } else {
-        return dir === "asc" ? -1 : 1;
-      }
-    });
+  function sorting(e) {
+    if (e.currentTarget.dataset.sortDirection === "asc") {
+      e.target.dataset.sortDirection = "desc";
+      setDirection(-1);
+    } else {
+      e.target.dataset.sortDirection = "asc";
+      setDirection(1);
+    }
+
+    setSortBy(e.currentTarget.dataset.sort);
   }
-  sortBeers();
+
+  function sortBeers(a, b) {
+    if (a[sortBy] > b[sortBy]) {
+      return 1 * direction;
+    } else {
+      return -1 * direction;
+    }
+  }
+
+  let displayList = [...props.beers].sort(sortBeers);
+  const beers = displayList.map((beer) => <Product addToBasket={props.addToBasket} addMoreBeer={props.addMoreBeer} key={beer.id} {...beer} />);
+
   return (
     <section className="ProductList">
       <h2>Our selection</h2>
-      <button onClick={() => setSort("name")}>Name</button>
-      <button onClick={() => setSort("category")}>Type</button>
-      <button onClick={() => setSort("alcohol")}>% alcohol</button>
+      <div className="sortingOptions">
+        <h4>Sorty by:</h4>
+        <button data-sort-direction="asc" data-sort="name" onClick={sorting}>
+          Name
+        </button>{" "}
+        |
+        <button data-sort-direction="asc" data-sort="category" onClick={sorting}>
+          Type
+        </button>{" "}
+        |
+        <button data-sort-direction="asc" data-sort="alc" onClick={sorting}>
+          % alcohol
+        </button>
+      </div>
       <section>{beers}</section>
     </section>
   );
