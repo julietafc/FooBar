d85a8ed7c857dc2067c8583b2e5934d3d919a15a
julietafc <juli47g1@stud.kea.dk>
2021-12-07 11:21:33 +0100

    confetti in bartender

--- src/components/Bartender/Bartender.jsx
+++ src/components/Bartender/Bartender.jsx
@@ -28,7 +28,7 @@ export default function Bartender(props) {
 
   return (
     <div className="Bartender Layout">
-      <TimeToClose now={props.now} setIsHappyHour={props.setIsHappyHour} />
+      <TimeToClose now={props.now} isHappyHour={props.isHappyHour} setIsHappyHour={props.setIsHappyHour} />
       <Taps bartenders={props.bartenders} taps={props.taps} />
       <Depot {...props} />
       <OrderList {...props} />
