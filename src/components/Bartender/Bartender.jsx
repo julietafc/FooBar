bfc739ad95c7ef640136c9847df31a1d4d77f6bf
Miguel <migu0403@stud.kea.dk>
2021-12-08 10:24:58 +0100

    sort working in inventory

--- src/components/Bartender/Bartender.jsx
+++ src/components/Bartender/Bartender.jsx
@@ -3,21 +3,58 @@ import Taps from "../helpers/Taps/Taps";
 import React from "react";
 import TimeToClose from "../helpers/TimeToClose/TimeToClose";
 import OrderList from "../helpers/OrderList/OrderList";
+import Bulb from "../helpers/Bulb/Bulb";
+import { useState } from "react";
 
 function Keg(props) {
   return (
     <li>
-      <div className={props.amount <= 2 ? (props.amount < 2 ? "light turn-on" : "light half") : "light"}></div>
+      <Bulb {...props} />
       {props.name}: {props.amount} left
     </li>
   );
 }
 
 function Depot(props) {
-  const kegs = props.storage.map((keg, i) => <Keg key={(i < 9 ? "keg0" : "keg") + (i + 1)} {...keg} />);
+  const [sortBy, setSortBy] = useState("amount");
+  const [direction, setDirection] = useState(1);
+
+  function sorting(e) {
+    if (e.currentTarget.dataset.sortDirection === "asc") {
+      e.target.dataset.sortDirection = "desc";
+      setDirection(-1);
+    } else {
+      e.target.dataset.sortDirection = "asc";
+      setDirection(1);
+    }
+
+    setSortBy(e.currentTarget.dataset.sort);
+  }
+
+  function sortBeers(a, b) {
+    if (a[sortBy] > b[sortBy]) {
+      return 1 * direction;
+    } else {
+      return -1 * direction;
+    }
+  }
+
+  let displayList = [...props.storage].sort(sortBeers);
+
+  const kegs = displayList.map((keg, i) => <Keg key={(i < 9 ? "keg0" : "keg") + (i + 1)} {...keg} />);
   return (
     <section className="Depot">
       <h2>Inventory</h2>
+      <div className="sorting-wrapper">
+        <h4>{direction === 1 ? "↓ " : "↑ "} Sorty by:</h4>
+        <button data-sort-direction="asc" data-sort="amount" onClick={sorting} style={sortBy === "amount" ? { color: "red" } : { color: "white" }}>
+          Amount
+        </button>
+        |
+        <button data-sort-direction="asc" data-sort="name" onClick={sorting} style={sortBy === "name" ? { color: "red" } : { color: "white" }}>
+          Name
+        </button>
+      </div>
       <ul>{kegs}</ul>
     </section>
   );
