b74d67bb1f98a961e8936c2594184695dd4af516
Miguel <migu0403@stud.kea.dk>
2021-12-11 00:23:54 +0100

    style customer

--- src/components/Customer/Customer.jsx
+++ src/components/Customer/Customer.jsx
@@ -1,13 +1,14 @@
 import "./Customer.scss";
 import TimeToClose from "../helpers/TimeToClose/TimeToClose";
+import React, { useEffect } from "react";
 
 function Beers(props) {
   const allOrders = [...props.serving, ...props.queue];
   const displayBeers = allOrders.map(function (order) {
-    const style = {
-      width: `${order.order.length * 10 + 50}px`,
-    };
-    return <div key={order.id} className={"beerPx " + order.id} style={style}></div>;
+    // const style = {
+    //   width: `${order.order.length * 10 + 50}px`,
+    // };
+    return <div key={order.id} className={"beerPx " + order.id}></div>;
   });
   return <section className="Beers">{displayBeers}</section>;
 }
@@ -17,6 +18,17 @@ export default function Customer(props) {
     return null;
   }
 
+  useEffect(() => {
+    document.querySelector("#fullscreen").addEventListener("click", () => {
+      document
+        .querySelector(".Customer")
+        .requestFullscreen()
+        .catch((e) => {
+          console.log(e);
+        });
+    });
+  });
+
   const readyMap = props.ordersReady.map((order) => order.id && <li key={order.id}>{order.id}</li>);
 
   return (
@@ -24,9 +36,10 @@ export default function Customer(props) {
       <TimeToClose now={props.now} isHappyHour={props.isHappyHour} setIsHappyHour={props.setIsHappyHour} />
       <Beers {...props} />
       <aside>
-        <h2>orders ready</h2>
+        <h2>Orders Ready</h2>
         <ul className="orders-ready">{readyMap}</ul>
       </aside>
+      <button id="fullscreen">◲</button>
     </div>
   );
 }
