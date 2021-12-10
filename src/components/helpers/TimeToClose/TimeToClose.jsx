9dd7ec2d80d7c10e679d26600d1a30bde72a07f6
Miguel <migu0403@stud.kea.dk>
2021-12-10 13:05:08 +0100

    time bar style

--- src/components/helpers/TimeToClose/TimeToClose.jsx
+++ src/components/helpers/TimeToClose/TimeToClose.jsx
@@ -1,5 +1,4 @@
 import "./TimeToClose.scss";
-import timeDiference from "../../../modules/timeDiference";
 import settingTime from "../../../modules/settingTime";
 import TimeBar from "../TimeBar/TimeBar";
 import React, { useEffect } from "react";
@@ -17,8 +16,6 @@ export default function TimeToClose(props) {
   const happyHourEnd = settingTime(happyHourTime + 1);
   const closingTime = settingTime(closingHour);
   const openingTime = settingTime(openHour);
-  const timeLeft = timeDiference(props.now, closingTime);
-  const timeDisplay = `${timeLeft.hours}:${timeLeft.minutes}:${timeLeft.seconds}`;
 
   useEffect(() => {
     if (props.now > happyHourStar && props.now < happyHourEnd) {
@@ -33,11 +30,9 @@ export default function TimeToClose(props) {
   return (
     <>
       <article className="TimeToClose">
-        <TimeBar isBarActive={isOpen} timeInit={openingTime} timeEnd={closingTime} now={props.now} label="TIME LEFT" />
         <TimeBar isBarActive={props.isHappyHour} timeInit={happyHourStar} timeEnd={happyHourEnd} now={props.now} label="HAPPY HOUR" />
-
+        <TimeBar isBarActive={isOpen} timeInit={openingTime} timeEnd={closingTime} now={props.now} label="TIME LEFT" />
         {props.isHappyHour && props.now < happyHourStar + 30000 && <Confetti width={window.innerWidth} height={window.innerHeight} />}
-        {/* <h2>{props.isHappyHour ? "HAPPY HOUR" : "TIME"}</h2> */}
       </article>
     </>
   );
