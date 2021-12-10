818fccc055ad03f41d175b80e1b735bf826d8c0b
Miguel <migu0403@stud.kea.dk>
2021-12-10 01:10:38 +0100

    beers que

--- src/components/Customer/Customer.jsx
+++ src/components/Customer/Customer.jsx
@@ -2,6 +2,12 @@ import "./Customer.scss";
 import TimeToClose from "../helpers/TimeToClose/TimeToClose";
 import timeDiference from "../../modules/timeDiference";
 
+function Beers(props) {
+  const allOrders = [...props.serving, ...props.queue];
+  const displayBeers = allOrders.map((order) => <div key={order.id} className="beerPx" style={{ width: `${order.order.length * 10 + 50}px` }}></div>);
+  return <section className="Beers">{displayBeers}</section>;
+}
+
 export default function Customer(props) {
   if (!props.taps) {
     return null;
@@ -22,12 +28,13 @@ export default function Customer(props) {
   const readyMap = props.ordersReady.map((order) => order.id && <li key={order.id}>{order.id}</li>);
 
   return (
-    <div className="Customer Layout">
+    <div className="Customer">
       <TimeToClose now={props.now} isHappyHour={props.isHappyHour} setIsHappyHour={props.setIsHappyHour} />
-      <section>
+      <Beers {...props} />
+      <aside>
         <h2>orders ready</h2>
         <ul className="orders-ready">{readyMap}</ul>
-      </section>
+      </aside>
     </div>
   );
 }
