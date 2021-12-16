156cb642c8e0bb513b853ce0aaa5f3565e9490d9
julietafc <juli47g1@stud.kea.dk>
2021-12-16 01:27:19 +0100

    final header

--- src/components/helpers/Header/Header.jsx
+++ src/components/helpers/Header/Header.jsx
@@ -1,20 +1,12 @@
 import { useState, useEffect } from "react";
-import { NavLink } from "react-router-dom";
-import Nav1 from "../../helpers/Nav1/Nav1";
-import { FaHeart } from "react-icons/fa";
+import Nav from "../Nav/Nav";
 import "./Header.scss";
-
 export default function Header(props) {
   const [windowDimension, setWindowDimension] = useState(null);
-  const [isCustomer, setIsCustomer] = useState(false);
+
   const [click, setClick] = useState(false);
   const handleClick = () => setClick(!click);
   const closeMobileMenu = () => setClick(false);
-  //   const [cart, setCart] = useState(false);
-
-  //   function changeCartState(state) {
-  //     setCart(state);
-  //   }
 
   useEffect(() => {
     setWindowDimension(window.innerWidth);
@@ -32,83 +24,52 @@ export default function Header(props) {
   const isMobile = windowDimension <= 640;
 
   return (
-    <>
-      {isCustomer ? (
-        <header className="mobileHeader">
-          <div className="logo"></div>
-          <Nav1 isMobile={isMobile} cart={props.cart} changeCartState={props.changeCartState} />
-        </header>
-      ) : (
-        <header>
-          <div className="logo">
-            <a href="">
-              <img src="/assets/logo-02.svg" alt="" />
-            </a>
-          </div>
+    <header className={props.isCustomer ? "mobileHeader" : "null"}>
+      <div className="logo">
+        <a href="/">
+          <img src={props.isCustomer && isMobile ? "/assets/logo-02-black.svg" : "/assets/logo-02.svg"} alt="logo foobar" />
+        </a>
+      </div>
+      <Nav isMobile={isMobile} cart={props.cart} changeCartState={props.changeCartState} isCustomer={props.isCustomer} click={click} handleClick={handleClick} closeMobileMenu={closeMobileMenu} />
+      {/* <nav className="navigation">
+        <ul className={click ? "nav-options active" : "nav-options"}>
+          <li className="option" onClick={closeMobileMenu}>
+            <NavLink to="/">Home</NavLink>
+          </li>
 
-          <nav className="navigation">
-            <ul className={click ? "nav-options active" : "nav-options"}>
-              <li className="option" onClick={closeMobileMenu}>
-                <NavLink to="/">Home</NavLink>
-              </li>
+          {!props.isCustomer && (
+            <li className="option" onClick={closeMobileMenu}>
+              <NavLink to="/Manager">Manager</NavLink>
+            </li>
+          )}
 
-              <li className="option" onClick={closeMobileMenu}>
-                <NavLink to="/Manager">Manager</NavLink>
-              </li>
-              <li className="option" onClick={closeMobileMenu}>
-                <NavLink to="/Bartender">Bartenders</NavLink>
-              </li>
-              <li className="option" onClick={closeMobileMenu}>
-                <NavLink
-                  to="/Dashboard"
-                  onClick={() => {
-                    setIsCustomer(true);
-                  }}
-                >
-                  Customers
-                </NavLink>
-              </li>
-            </ul>
-            <div className="mobile-menu" onClick={handleClick}>
-              {click ? "X" : "☰"}
-            </div>
-            {/* <NavLink to="/">Home</NavLink>
-            <NavLink to="/Manager">Manager</NavLink>
-            <NavLink to="/Bartender">Bartenders</NavLink>
-            <NavLink
-              to="/Dashboard"
-              onClick={() => {
-                setIsCustomer(true);
-              }}
-            >
-              Customers
-            </NavLink> */}
-          </nav>
-        </header>
-      )}
-    </>
+          {!props.isCustomer && (
+            <li className="option" onClick={closeMobileMenu}>
+              <NavLink to="/Bartender">Bartenders</NavLink>
+            </li>
+          )}
+
+          <li className="option" onClick={closeMobileMenu}>
+            <NavLink to="/Dashboard">{!props.isCustomer ? "Customers" : "Dashboard"}</NavLink>
+          </li>
+
+          {props.isCustomer && (
+            <li className="option" onClick={closeMobileMenu}>
+              <NavLink
+                to="/Form"
+                onClick={() => {
+                  props.changeCartState(!props.cart);
+                }}
+              >
+                {props.cart ? "Menu" : isMobile ? "Cart" : "Menu"}
+              </NavLink>
+            </li>
+          )}
+        </ul>
+        <div className="mobile-menu" onClick={handleClick}>
+          <button className="btn-burger">{click ? "X" : "☰"}</button>
+        </div>
+      </nav> */}
+    </header>
   );
-}
-
-{
-  /* <header>
-<h1>FooBar</h1>
-
-<nav className="navigation">
-  <NavLink to="/">Home</NavLink>
-  {!isCustomer && <NavLink to="/Manager">Manager</NavLink>}
-  {!isCustomer && <NavLink to="/Bartender">Bartenders</NavLink>}
-  <NavLink to="/Dashboard">{!isCustomer ? "Customers" : "Dashboard"}</NavLink>
-  {isCustomer && (
-    <NavLink
-      to="/Form"
-      onClick={() => {
-        changeCartState(!cart);
-      }}
-    >
-      {cart ? "Order" : isMobile ? "Cart" : "Order"}
-    </NavLink>
-  )}
-</nav>
-</header> */
 }
