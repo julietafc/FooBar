19bfd2cfb38014211487976501d2da8b88ff1def
julietafc <juli47g1@stud.kea.dk>
2022-01-10 14:41:49 +0100

    log in set up

--- src/components/Manager.jsx
+++ src/components/Manager.jsx
@@ -3,10 +3,17 @@ import Inventory from "./helpers/Inventory";
 import Workers from "./helpers/Workers";
 import TopBeer from "./helpers/Ranking";
 import Sales from "./helpers/Sales";
+import React, { useState, useEffect } from "react";
+import Login from "../components/Login";
 
 export default function Manager(props) {
+  const [token, setToken] = useState();
   if (!props.bartenders) {
     return null;
+  }
+  if (!token) {
+    return <Login setToken={setToken} />;
+  } else {
   }
 
   return (
